e5f8fc56dc924dd9717a6a7b69c24512b9a91e29
TerryMinal <terryguan513@gmail.com>
2018-03-24 12:05:23 -0400

    graphing working

--- fed.js
+++ fed.js
@@ -5,11 +5,14 @@ var src = 'http://www.w3.org/2000/svg';
 var y2015 = [2.96, 3.36, -0.339, 16.2]
 var y2016 = [2.99, 3.54, -0.552, 16.5]
 
+
 var graph  = function(l) {
-  c = d3.select(".chart");
-	setup = c.selectAll("div").data(l).enter().append("div").style("width", function(d){
-  return d * 100 + "px;"; });
-  console.log(setup);
+var chart = d3.select(".chart").selectAll("div").data(l).enter().append("div");
+ //Set width of each bar proportional to its data value.
+ chart.style("width", function(d) {
+ return d * 10 + "px"; });
+ //Label each bar.
+ chart.text(function(d) { return d; });
 }
 
 graph(y2015);
